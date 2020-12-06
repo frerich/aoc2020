843ea8ad8eed3b0678db793db43b9d7b635464b5
Frerich Raabe <raabe@froglogic.com>
2020-12-06 11:44:04 +0100

    Day 6 solutions in Python

---
+++ day6.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def parse(group):
+    return [set(answers) for answers in group.rstrip().split('\n')]
+
+
+if __name__ == '__main__':
+    groups = [parse(group) for group in sys.stdin.read().split('\n\n')]
+
+    print("Part one:", sum(len(set.union(*g)) for g in groups))
+    print("Part two:", sum(len(set.intersection(*g)) for g in groups))
