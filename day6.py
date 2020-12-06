56aef41e91c0ed12b87a61ba4dc1de22a66b92af
Frerich Raabe <raabe@froglogic.com>
2020-12-06 11:51:43 +0100

    Minor improvement for day 6 solution in Python
    The 'rstrip()' call shuold not be in 'parse', it shuold be done in the
    same place where read() is called.

--- day6.py
+++ day6.py
@@ -2,11 +2,11 @@ import sys
 
 
 def parse(group):
-    return [set(answers) for answers in group.rstrip().split('\n')]
+    return [set(answers) for answers in group.split('\n')]
 
 
 if __name__ == '__main__':
-    groups = [parse(group) for group in sys.stdin.read().split('\n\n')]
+    groups = [parse(group) for group in sys.stdin.read().rstrip().split('\n\n')]
 
     print("Part one:", sum(len(set.union(*g)) for g in groups))
     print("Part two:", sum(len(set.intersection(*g)) for g in groups))
