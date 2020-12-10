a2e82df07e88fd92f78c71f3f90bb692778c3e21
Frerich Raabe <raabe@froglogic.com>
2020-12-10 08:58:12 +0100

    Extend Day 8 solution in Python with GraphViz helper
    This little function generates a .gv file for a directed graph
    representing the control flow of the program.

--- day8.py
+++ day8.py
@@ -51,6 +51,27 @@ def partTwo(program):
         program[i] = (cmd, arg)
 
 
+def dotLabel(program, addr):
+    cmd, arg = program[addr]
+    return "{:04x}: {} {:+d}".format(addr, cmd, arg)
+
+def dotGraph(program):
+    lines = []
+    lines.append("digraph {")
+    for i, (cmd, arg) in enumerate(program[:-1]):
+        thisLabel = dotLabel(program, i)
+        if cmd == 'jmp':
+            lines.append('"{}" -> "{}" [style=dashed];'.format(dotLabel(program, i), dotLabel(program, i + 1)))
+            lines.append('"{}" -> "{}";'.format(dotLabel(program, i), dotLabel(program, i + arg)))
+        elif cmd == 'nop':
+            lines.append('"{}" -> "{}";'.format(dotLabel(program, i), dotLabel(program, i + 1)))
+            lines.append('"{}" -> "{}" [style=dashed];'.format(dotLabel(program, i), dotLabel(program, i + arg)))
+        elif cmd == 'acc':
+            lines.append('"{}" -> "{}";'.format(dotLabel(program, i), dotLabel(program, i + 1)))
+    lines.append("}")
+
+    return '\n'.join(lines)
+
 def parse(line):
     cmd, arg = line.split()
     return cmd, int(arg)
